93bb360e23b63d45c6e618745bb121eb6ae75e05
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2026-06-18 20:05:25 +0200

    Apply suggestions from code review
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Client/Kudzu/KudzuVisualizerComponent.cs
+++ Content.Client/Kudzu/KudzuVisualizerComponent.cs
@@ -1,8 +1,11 @@
+using Content.Shared.Spreader;
+
 namespace Content.Client.Kudzu;
 
 /// <summary>
 ///     Entities that have a visual state corresponding to current growth level
 ///     and random variation, for kudzu tiles.
+///    <seealso cref="KudzuVisuals"/>
 /// </summary>
 [RegisterComponent]
 public sealed partial class KudzuVisualsComponent : Component
@@ -10,6 +13,6 @@ public sealed partial class KudzuVisualsComponent : Component
     /// <summary>
     ///     The index of the sprite layer that is reflecting the kudzu's growth state.
     /// </summary>
-    [DataField("layer")]
+    [DataField]
     public int Layer { get; private set; } = 0;
 }
