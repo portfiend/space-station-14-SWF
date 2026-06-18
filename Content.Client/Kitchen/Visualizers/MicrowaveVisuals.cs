dbd37117cb5e2a6e22491c950697667eb7cbc8ee
portfiend <109661617+portfiend@users.noreply.github.com>
2026-06-18 11:10:30 -0500

    i comment it too

--- Content.Client/Kitchen/Visualizers/MicrowaveVisuals.cs
+++ Content.Client/Kitchen/Visualizers/MicrowaveVisuals.cs
@@ -1,8 +1,18 @@
 namespace Content.Client.Kitchen.Visualizers;
 
+/// <summary>
+///     Visual layers for a microwave.
+/// </summary>
 public enum MicrowaveVisualizerLayers : byte
 {
+    /// <summary>
+    ///     The "base" of the microwave.
+    /// </summary>
     Base,
+
+    /// <summary>
+    ///     The unshaded display panel on the microwave.
+    /// </summary>
     BaseUnlit
 }
 
